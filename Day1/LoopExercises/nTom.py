5c986503e9d23e3c8211f5c99d5da5a7aa932a12
Christian Lowe <christglowe@gmail.com>
2018-02-06 15:16:09 -0600

    Added some of the loop exercises

---
+++ Day1/LoopExercises/nTom.py
@@ -0,0 +1,5 @@
+begin = int(input("Please Select a number to start on: "))
+end = int(input("Please Select a number to end on: "))
+
+for i in range(begin, end + 1):
+    print(i)
---
+++ Day1/LoopExercises/printSquare.py
@@ -0,0 +1,10 @@
+
+
+def printSquareWithSpaces(size):
+    for i in range (0, size):
+        for j in range (0, size):
+            print("*", end=" ")
+        print(" ")
+    
+
+printSquare(5)
