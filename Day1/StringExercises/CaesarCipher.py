732b62ca7c78e48be2311f061d71421ba19a74d9
Christian Lowe <christglowe@gmail.com>
2018-02-06 14:59:49 -0600

    Finished caesar cipher implementation

--- Day1/StringExercises/CaesarCipher.py
+++ Day1/StringExercises/CaesarCipher.py
@@ -1,12 +1,24 @@
-userString = str(input("Give me a string you'd like to encrypt: "))
+import math
 
-def cipher(string):
+userString = str(input("Give me a string you'd like to encrypt: ")).upper()
+userKey = int(input("Give me an encryption key (number between 1-26): "))
+
+def cipher(string, key):
     encryptedString = ""
     placeholder = 0
     for i in string:
         if i != " " or i != "." or i != "!" or i != "?":
             placeholder = ord(i) 
-            print(placeholder)
+           
+            if placeholder + key > 91:
+                placeholder = (((placeholder - 65) + key) % 26) + 65
+                print(placeholder)
+            else:
+                placeholder = placeholder + key
+                
+                print(placeholder)
+            encryptedString += chr(placeholder)
+    return encryptedString
 
 
-cipher(userString)
+print(cipher(userString, userKey))
