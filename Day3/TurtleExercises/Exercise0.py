4087fa53a7308e4c1f38432282b0408b05b2d41b
Christian Lowe <christglowe@gmail.com>
2018-02-07 15:37:05 -0600

    Added a few more exercises from the Turtle challenges

---
+++ Day3/TurtleExercises/Exercise0.py
@@ -0,0 +1,27 @@
+from turtle import *
+
+# forward(100)
+# right(90)
+# forward(100)
+# right(90)
+# forward(100)
+# right(90)
+# forward(100)
+
+
+# for i in range(6):
+#     right(216)
+#     forward(150)
+
+
+#draw a cirlce
+up()
+forward(50)
+left(90)
+forward(50)
+left(90)
+
+pencolor('#005ce6')
+width(20)
+circle(160)
+mainloop()
---
+++ Day3/TurtleExercises/Exercise1.py
@@ -0,0 +1,16 @@
+from turtle import *
+
+
+#draw a triangle
+# right(120)
+# forward(200)
+# right(120)
+# forward(200)
+# right(120)
+# forward(200)
+
+
+
+#draw a pentagon
+
+mainloop()
