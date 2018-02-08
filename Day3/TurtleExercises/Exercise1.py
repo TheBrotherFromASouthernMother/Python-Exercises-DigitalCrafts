36205b4e1466c7157b45129973ec05fb511ed76f
Christian Lowe <christglowe@gmail.com>
2018-02-08 12:10:25 -0600

    Finished polygon drawing for exercise1

--- Day3/TurtleExercises/Exercise1.py
+++ Day3/TurtleExercises/Exercise1.py
@@ -11,26 +11,50 @@ shape("turtle")
 # forward(200)
 
 
+def setPosition(direction):
+    direction = direction.lower()
+    if direction == "left":
+        penup()
+        goto(-200, -200)
+        down()
+    elif direction == 'right':
+        penup()
+        goto(200, -200)
+        down()
 
 
 #draw a pentagon
-def drawPolygon(numberSide, sideLength, name):
+def drawPolygon(numberSide, sideLength, name, firstShape=False, lastShape=False, someFunction=None, direction=None):
+    if someFunction:
+        someFunction(direction)
     numSide = numberSide #5
     side_length = sideLength #70
     angle = 360 / numSide 
-    home()
+    if firstShape == True:
+        home()
     begin_poly()
     for i in range(numSide):
-        fd(side_length)
+        forward(side_length)
         left(angle)
-        fd(side_length)
+        forward(side_length)
     end_poly()
     p = get_poly()
     register_shape(name, p)
-    mainloop()
+    if lastShape:
+        mainloop()
+    
 
-pentagon = drawPolygon(5, 70, "pentagon")
+pentagon = drawPolygon(5, 30, "pentagon", True)
+
+octagon = drawPolygon(8, 30, "octagon", False, False, setPosition, "right" )
+
+#draw a hexagon
+hexagon = drawPolygon(6, 30, "hexagon",False, True, setPosition, "left")
+
+print("trueifthing")
 
 if __name__ == "__main__":
-        pentagon()
+    pentagon()
+    octagon()
+    hexagon()
         
