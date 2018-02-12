d64c87ccdb25afb76f772b19047343683dfbad39
Christian Lowe <christglowe@gmail.com>
2018-02-12 15:25:32 -0600

    Modified saveEntry function giving it a dictionary parameter so as to make it more modular

--- Day6/phonebook.py
+++ Day6/phonebook.py
@@ -1,4 +1,5 @@
 import json
+import os.path
 
 mainMenu = {
     'header' : 'Electronic Phone Book\n=====================',
@@ -9,37 +10,6 @@ mainMenu = {
     "5": "Quit"  
 }
 
-phonebook_dict = {
-    "Chris": {
-        "name": "Chris",
-        "phone_number": "7"
-    }
-}
-
-def saveEntry():
-    name = str(input("Name? "))
-    phone_number = str(input("Phone Number? "))
-    phonebook_dict[name] = {
-        'name': name,
-        "phone_number": phone_number
-        }
-    print("Added entry:\n \'name: {}\nphone: {}\'\nto phonebook". \
-    format(phonebook_dict[name]['name'], phonebook_dict[name]['phone_number']), "\n\n")
-
-def printAllEntries(dictionary):
-    for key in dictionary:
-        print(key, dictionary[key], "\n")
-
-def deleteEntry(dictionary):
-    entryToDelete = str(input("Please input the contact name you would like to delete: "))
-    if dictionary[entryToDelete]:
-        del dictionary[entryToDelete]
-
-def lookUpEntry(dictionary):
-    contactToLookUp = str(input("Please input a name you would like to find: "))
-    if dictionary[contactToLookUp]:
-        print(dictionary[contactToLookUp]['name'])
-        print(dictionary[contactToLookUp]['phone_number'])
 
 
 def showMenu():
@@ -51,12 +21,49 @@ def showMenu():
             print(key, mainMenu[key])
     print("\n")
 
+
+def lookUpEntry(dictionary):
+    contactToLookUp = str(input("Please input a name you would like to find: "))
+    if dictionary[contactToLookUp]:
+        print(dictionary[contactToLookUp]['name'])
+        print(dictionary[contactToLookUp]['phone_number'])
+
+
+def saveEntry(dictionary):
+    name = str(input("Name? "))
+    phone_number = str(input("Phone Number? "))
+    dictionary[name] = {
+        'name': name,
+        "phone_number": phone_number
+        }
+    print("Added entry:\n \'name: {}\nphone: {}\'\nto phonebook". \
+    format(dictionary[name]['name'], dictionary[name]['phone_number']), "\n\n")
+
+
+def deleteEntry(dictionary):
+    entryToDelete = str(input("Please input the contact name you would like to delete: "))
+    if dictionary[entryToDelete]:
+        del dictionary[entryToDelete]
+
+
+
+def printAllEntries(dictionary):
+    for key in dictionary:
+        print(key, dictionary[key], "\n")
+
+
+def saveEntriesToFile():
+    with open('phonebook.json', 'w') as myFile:
+        json.dump(phonebook_dict, myFile)
+
+
 def userRoute():
     userCmd = int(input('What would you like to do? (1-5) '))
     while userCmd < 0 or userCmd > 5:
         userCmd = int(input('What would you like to do? (1-5) '))
     if userCmd == 5:
         print('Exiting... \n')
+        saveEntriesToFile()
         return 0
     elif userCmd == 4:
         printAllEntries(phonebook_dict)
@@ -64,7 +71,7 @@ def userRoute():
         deleteEntry(phonebook_dict)
         print(phonebook_dict)
     elif userCmd == 2:
-        saveEntry()
+        saveEntry(phonebook_dict)
     elif userCmd == 1:
         lookUpEntry(phonebook_dict)
     
@@ -73,6 +80,18 @@ def userRoute():
     
 
 def initializePhoneBook():
+    global phonebook_dict
+    if os.path.isfile('phonebook.json'):
+        with open('phonebook.json', 'r') as myFile:
+            phonebook_dict = json.load(myFile)
+    else:
+        phonebook_dict = {
+                "Chris": {
+                    "name": "Chris",
+                    "phone_number": "7"
+                }
+            }
+
     while True:
         showMenu()
         if userRoute() == 0:
